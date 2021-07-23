37d313fe6f8123e8db85dd086436d49839c6d8ce
Daniel Popescu <same.dan@gmail.com>
2021-07-23 11:52:30 +0200

    Delete Rentals

--- controllers/rentals.js
+++ controllers/rentals.js
@@ -1,4 +1,5 @@
 const Rental = require("../models/rental");
+const Booking = require("../models/booking");
 
 exports.getRentals = async (req, res) => {
   const { city } = req.query;
@@ -34,16 +35,9 @@ exports.getRentalById = async (req, res) => {
   } catch (error) {
     return res.mongoError(error);
   }
-
-  // Rental.findById(rentalId, (error, foundRental) => {
-  //   if (error) {
-  //     return res.mongoError(error);
-  //   }
-
-  //   return res.json(foundRental);
-  // });
 };
 
+// POST
 exports.createRental = (req, res) => {
   const rentalData = req.body;
   rentalData.owner = res.locals.user;
@@ -54,6 +48,33 @@ exports.createRental = (req, res) => {
     }
     return res.json(createdRental);
   });
+};
+
+//DELETE /api/v1/rentals/A5gk7_ç3
+exports.deleteRental = async (req, res) => {
+  const { rentalId } = req.params;
+  const { user } = res.locals;
+  try {
+    const rental = await Rental.findById(rentalId).populate("owner");
+    const bookings = await Booking.find({ rental });
+    if (user.id !== rental.owner.id) {
+      return res.sendApiError({
+        title: "Invalid user",
+        detail: "You don't have the credentials to delete this rental",
+      });
+    }
+    // TODO - delete rental with bookings in the past
+    if (bookings && bookings.length > 0) {
+      return res.sendApiError({
+        title: "Active Bookings",
+        detail: "Cannot delete rental with active bookings",
+      });
+    }
+    await rental.remove();
+    return res.json({ id: rentalId });
+  } catch (error) {
+    return res.mongoError(error);
+  }
 };
 
 //middlewares
